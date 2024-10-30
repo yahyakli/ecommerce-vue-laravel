7e14c716902e61b162c93568880a9cc1424307b9
yahya akli <akli.yahya05@gmail.com>
2024-10-30 13:36:23 +0100

    main

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'email_verified_at',
+        'is_admin'
     ];
 
     /**
@@ -33,15 +36,16 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function customer()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasOne(Customer::class);
     }
 }
--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
+import store from './store'
+import router from './router'
+import './index.css';
+import currencyUSD from './filters/currency.js'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app
+  .use(store)
+  .use(router)
+  .mount('#app')
+;
+
+app.config.globalProperties.$filters = {
+  currencyUSD
+}
